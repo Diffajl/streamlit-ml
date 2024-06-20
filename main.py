80953f6f7bc9b58752ff1755e41553867fcef623
Diffajl <147916548+Diffajl@users.noreply.github.com>
2024-06-21 06:22:38 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,98 @@
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# from sklearn.datasets import load_iris
+# from sklearn.ensemble import RandomForestClassifier
+
+# st.write("""
+# Simple Iris FLower Prediction App
+         
+# This app predicts the iris flower type!
+# """)
+
+# st.sidebar.header("User input parameters")
+
+# def user_input_features():
+#     sepal_length = st.sidebar.slider("Sepal_length", 4.3, 7.9, 5.4)
+#     sepal_width = st.sidebar.slider("Sepal_width", 2.0, 3.3, 3.4)
+#     petal_length = st.sidebar.slider("Petal_length", 1.0, 6.9, 1.3)
+#     petal_width = st.sidebar.slider("Petal_width", 0.1, 2.5, 0.2)
+
+#     features = pd.DataFrame({
+#         'sepal_length': sepal_length,
+#         'sepal_width': sepal_width,
+#         'petal_length': petal_length,
+#         'petal_width': petal_width,
+#     }, index=[0])
+
+#     return features
+
+# df = user_input_features()
+# st.subheader("User input parameters")
+# st.write(df)
+
+# iris = load_iris()
+# X = iris.data
+# y = iris.target
+
+# model = RandomForestClassifier()
+# model.fit(X, y)
+
+# prediction = model.predict(df)
+# prediction_proba = model.predict_proba(df)
+
+# st.subheader("Class labels and their corresponding index number")
+# st.write(iris.target_names)
+
+# st.subheader("Prediction Probability")
+# st.write(prediction_proba)
+
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Simple Iris Flower Prediction App
+
+This app predicts the **Iris flower** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
